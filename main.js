da13de0c9cdc7985300d05c907675e58f97e5424
rotem huga <rotem.3007@gmail.com>
2023-01-07 22:16:21 +0200

    dom practice

---
+++ main.js
@@ -0,0 +1,86 @@
+// // //crocoder - EX2
+// // //fruit list
+// // //1
+// // // const fruitList = ["apple", "banana", "tomato"];
+// // // const fruitUl = document.querySelector("fruitList");
+// // // fruitList.forEach((fruit) => {
+// // //     const itemElement = document.createElement("li");
+// // //     itemElement.textContent = fruit;
+// // //     fruitUl.appendChild(itemElement);
+// // // });
+
+// // const fruitList = ["apple", "banana", "tomato"];
+// // const newUl = document.getElementById("fruitList");
+// // function listElement ();
+// //     for (let i = 0; i < fruitList.length; i++) {
+// //         const itemElement = document.createElement("li");
+// //         itemElement.textContent = i;
+// //         newUl.appendChild(itemElement);
+// //     }
+
+// // listElement ()
+// // //2
+// // const imagesrc = "https://crocoder.dev/icon.png";
+// // const divElement = document.querySelector("div");
+// // const imgElement = document.createElement("img");
+// // imgElement.src = imagesrc;
+// // divElement.appendChild(imagesrc);
+
+// // //3
+// // const firstList = [...document.querySelectorAll("ul > li:first-child")];
+// // const lastList = [...document.querySelecto rAll("ul > li:last-child")];
+// // firstList.forEach((li) => (li.textContent = "first"));
+// // lastList.forEach((li) => (li.textContent = "last"));
+
+// //EX2 
+    //1
+    // let input = document.getElementById("input") 
+    // let button = document.getElementById("button")
+    
+    // button.addEventListener("click", ()=> {
+    //     const data = input.value;
+    //     document.querySelector("h2").innerText = data
+    // })
+
+    //2
+    // let paragraph = document.getElementById("para")
+    // let button = document.getElementById("counter")
+    // let numOfPress = 0
+    // button.addEventListener("click", () => {
+    //     numOfPress ++;
+    //     // paragraph.innerText = numOfPress
+    //     document.querySelector("p").innerText = numOfPress      
+    //     paragraph.style.fontSize = `${20+numOfPress}px`;
+    // })
+
+    //4
+    // window.onload = () => {
+    // const texts = ["Hello", "how are you?", "nice to meet you"];
+    // const randomIndex = Math.floor(Math.random() * texts.length);
+    // document.getElementById("heading").innerText = texts[randomIndex];
+    // };
+
+    //5
+    // let button = document.getElementById("submit")
+    // let input = document.getElementById("input")
+    // button.addEventListener("click", ()=> {
+    //      if (input.value === ""){
+    //         alert("error")
+    //      }
+    // })
+
+    //6
+    // let buttonHello = document.getElementById("hello") 
+    // buttonHello.addEventListener("click", ()=> {
+    //     alert("hello");
+    // })
+    // let buttonGoodbye = document.getElementById("goodbye") 
+    // buttonGoodbye.addEventListener("click", ()=> {
+    //     ale  rt("goodbye");
+    // })
+
+    //7
+//     const addToDo = document.getElementById("button");
+//     addToDo.addEventListener("click", () => {
+//         if (document.getElementById("input-box").value !== ""){
+// });
